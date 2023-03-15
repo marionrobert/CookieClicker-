6f35241ef8e30266b1a97e6fc96d468cf63d7cf0
Marion <mrobert148@gmail.com>
2023-03-15 17:49:05 +0100

    make dict of all items to buy with their price

--- main.py
+++ main.py
@@ -17,3 +17,23 @@ driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(By.ID, "cookie")
 cookie.click()
+
+
+
+def get_money_cookies():
+    return int((driver.find_element(By.ID, "money")).get_attribute("innerText"))
+
+
+# def get_store():
+store = {}
+all_items_names = ["Elder Pledge", "Time machine", "Portal", "Alchemy lab", "Shipment", "Mine", "Factory", "Grandma", "Cursor"]
+for item in all_items_names:
+    tag = driver.find_element(By.ID, f"buy{item}")
+    if item == "Elder Pledge":
+        store[item] = int(tag.get_attribute("innerText").split("Puts")[0].split("-")[1].replace(",", "").strip())
+    else:
+        store[item] = int(tag.get_attribute("innerText").split("\n")[0].split("-")[1].replace(",", "").strip())
+print(store)
+
+# while run_time > time.time_ns():
+#     cookie.click()
